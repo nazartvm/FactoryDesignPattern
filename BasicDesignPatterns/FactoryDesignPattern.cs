3cc78b01cd46478358b95867500d17ad9e5d925c
nazartvm <nisha@aktrea.com>
2018-08-01 06:55:19 +0530

    intial commit

---
+++ BasicDesignPatterns/FactoryDesignPattern.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicDesignPatterns
+{
+    /// <summary>
+    /// factory class which helps to create a object
+    /// </summary>
+    class FactoryDesignPattern
+    {
+        public Imember FactoryMethod(string type)
+        {
+            switch (type)
+            {
+                case "lifemember":
+                    return new LifeMember();
+                case "annualmember":
+                    return new AnnualMember();
+                case "tempmember":
+                    return new TempMember();
+            }
+            throw new Exception("pass correct member type");
+        }
+    }
+    /// <summary>
+    /// Interface which holds the member methods
+    /// </summary>
+    public interface Imember
+    {
+        void register();
+        void notify();
+    }
+    /// <summary>
+    /// Life member class
+    /// </summary>
+    public class LifeMember:Imember
+    {
+        public void register()
+        {
+            Console.WriteLine("I am  a life time member");
+        }
+        public void notify() {
+            Console.WriteLine("Life time member notified");
+        }
+    }
+    /// <summary>
+    /// annual member class
+    /// </summary>
+    public class AnnualMember : Imember
+    {
+        public void register()
+        {
+            Console.WriteLine("I am  a annual time member");
+        }
+        public void notify()
+        {
+            Console.WriteLine("Annual time member notified");
+        }
+    }
+    /// <summary>
+    /// Temp member class
+    /// </summary>
+    public class TempMember : Imember
+    {
+        public void register()
+        {
+            Console.WriteLine("I am  a temp time member");
+        }
+        public void notify()
+        {
+            Console.WriteLine("Temp time member notified");
+        }
+    }
+}
---
+++ BasicDesignPatterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryDesignPattern factory = new FactoryDesignPattern();
+            Imember member = factory.FactoryMethod("lifemember");
+            member.register();
+            Console.ReadLine();
+        }
+    }
+}
